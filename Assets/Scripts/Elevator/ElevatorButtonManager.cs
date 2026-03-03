adb8a33cac689fbd63fd54402d45eab715f22f60
Chaospeedy <seansdilday@gmail.com>
2026-03-02 22:58:21 -0600

    Manually added Elevator Scene
    due to merge problems I just manually recreated the whole elevator floor selector in this branch

--- Assets/Scripts/Elevator/ElevatorButtonManager.cs
+++ Assets/Scripts/Elevator/ElevatorButtonManager.cs
@@ -6,15 +6,58 @@ using UnityEngine.SceneManagement;
 
 public class ElevatorButtonManager : MonoBehaviour
 {
+
+    string sceneName; 
+    public float duration = 3.0f; 
+    private bool isMoving = false; 
+
+
+    [SerializeField] GameObject doorPart1;
+    [SerializeField] GameObject doorPart2;
     //Go to Main Floor on 1
-    public void OnFloorOneButton(){
-        SceneManager.LoadScene("LevelSelectorMainFloor");
+    public void OnFloor1Button(){
+        sceneName = "LevelSelectorMainFloor";
+        Invoke("ChangeFloor", 1f);
     }
 
     //Go to LevelSelector Floor 2 on 2
     public void OnFloor2Button(){
-        SceneManager.LoadScene("LevelSelector2ndFloor");
+        sceneName = "LevelSelector2ndFloor";
+        Invoke("ChangeFloor", 5f);
     }
 
-    //Can further implement for Floor 3 here and add more levels in the UI and include buttons!
+    private void ChangeFloor(){
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void OpenElevatorDoor(){
+        if (!isMoving)
+        {
+            StartCoroutine(MoveRoutine());
+        }
+    }
+
+    private IEnumerator MoveRoutine()
+    {
+        isMoving = true;
+        Vector3 startPosition1 = doorPart1.transform.position;
+        Vector3 startPosition2 = doorPart2.transform.position;
+        Vector3 endPosition1 = startPosition1 + new Vector3(15, 0, 0);
+        Vector3 endPosition2 = startPosition2 + new Vector3(10, 0, 0);
+        float elapsedTime = 0;
+
+        while (elapsedTime < duration)
+        {
+            // Use Vector3.Lerp to smoothly interpolate between start and end positions
+            doorPart1.transform.position = Vector3.Lerp(startPosition1, endPosition1, (elapsedTime / duration));
+            doorPart2.transform.position = Vector3.Lerp(startPosition2, endPosition2, (elapsedTime / duration));
+            elapsedTime += Time.deltaTime;
+            yield return null; // Wait until the next frame
+        }
+
+        // Ensure the object reaches the exact end position
+        doorPart1.transform.position = endPosition1;
+        doorPart2.transform.position = endPosition2;
+        isMoving = false;
+    }
 }

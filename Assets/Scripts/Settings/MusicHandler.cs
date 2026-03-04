a8da25d6ac29821d14d623c0be7941f74b58ad40
Chaospeedy <seansdilday@gmail.com>
2026-03-03 20:50:30 -0600

    Fixed music not playing in elevator

--- Assets/Scripts/Settings/MusicHandler.cs
+++ Assets/Scripts/Settings/MusicHandler.cs
@@ -7,12 +7,10 @@ public class MusicHandler : MonoBehaviour
 {
     
 
-    private AudioSource audioSource;
+    public AudioSource audioSource;
     public GlobalVariables GV;
     public AudioClip song;
-    //public AudioClip intro;
-    //public AudioClip loop;
-    //public double delayAdjust = 0f;
+
     void Start()
     {
         audioSource = GetComponentInChildren<AudioSource>();
@@ -24,33 +22,10 @@ public class MusicHandler : MonoBehaviour
         audioSource.loop = true;
         audioSource.Play();
 
-        //double startTime = AudioSettings.dspTime;
-
-        //audioSource.clip = intro;
-        //audioSource.PlayScheduled(startTime);
-
-        //double loopStartTime = startTime + intro.length - delayAdjust;
-        //audioSource.PlayScheduled(loopStartTime);
-       
-        //StartCoroutine(PlayIntroThenLoop(loopStartTime));
     }
     void Update()
     {
         audioSource.volume = GV.getAudioVolume() / 10f;
     }
-    /*private IEnumerator PlayIntroThenLoop(double time)
-    {
-        
-        double waitTime = time - AudioSettings.dspTime;
-        if(waitTime > 0)
-        {
-            yield return new WaitForSeconds((float)waitTime);
-        }
-        audioSource.clip = loop;
-        audioSource.loop = true;
-        audioSource.Play();
-    }*/
-
-
 
 }
